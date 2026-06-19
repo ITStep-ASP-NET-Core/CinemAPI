a83a38224be38f80859506e285e34e3713996b7f
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-19 04:45:47 +0200

    feat: implement initial infrastructure interfaces

---
+++ CinemAPI.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using CinemAPI.Domain.Common;
+using CinemAPI.Infrastructure.Data;
+using CinemAPI.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemAPI.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+		public async Task<PagedResult<T>> GetAllAsync ( int page, int pageSize = 2 )
+		{
+			var items = await _dbSet
+				.Skip(page * pageSize)
+				.Take(pageSize)
+				.AsNoTracking()
+				.ToListAsync();
+
+			return new PagedResult<T>
+			{
+				Items = items,
+				PageNumber = page,
+				PageSize = pageSize,
+				TotalCount = await _dbSet.CountAsync()
+			};
+		}
+
+		public async Task<T?> GetByIdAsync ( int id )
+		{
+			return await _dbSet.FindAsync(id);
+		}
+
+		public async Task AddAsync ( T obj )
+		{
+			await _dbSet.AddAsync(obj);
+		}
+
+		public void Update ( T obj )
+		{
+			_dbSet.Update(obj);
+		}
+
+		public void Delete ( T obj )
+		{
+			_dbSet.Remove(obj);
+		}
+	}
+}
---
+++ CinemAPI.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,97 @@
+﻿using CinemAPI.Domain.Common;
+using CinemAPI.Domain.Entities;
+using CinemAPI.Infrastructure.Data;
+using CinemAPI.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemAPI.Infrastructure.Repositories
+{
+	public class MovieRepository : IMovieRepository
+	{
+		private readonly ApplicationContext _context;
+
+		public MovieRepository ( ApplicationContext context )
+		{
+			_context = context;
+		}
+
+		public async Task<PagedResult<Movie>> GetMoviesAsync ( int page, int pageSize = 2 )
+		{
+			var items = await _context.Movies
+				.Skip(page * pageSize)
+				.Take(pageSize)
+				.AsNoTracking()
+				.ToListAsync();
+
+			return new PagedResult<Movie>
+			{
+				Items = items,
+				PageNumber = page,
+				PageSize = pageSize,
+				TotalCount = await _context.Movies.CountAsync()
+			};
+		}
+
+		public async Task<PagedResult<Movie>> GetMoviesByFiltersAsync (
+			string? searchQuery,
+			ICollection<int>? actorIds,
+			ICollection<int>? genreIds,
+			int page,
+			int pageSize = 2 )
+		{
+			var query = _context.Movies.AsQueryable();
+
+			if(!string.IsNullOrWhiteSpace(searchQuery))
+				query = query.Where(m => m.Title.ToLower().Contains(searchQuery.ToLower()));
+
+			if(actorIds != null && actorIds.Count > 0)
+				query = query.Where(m => m.Actors.Any(a => actorIds.Contains(a.Id)));
+
+			if(genreIds != null && genreIds.Count > 0)
+				query = query.Where(m => m.Genres.Any(g => genreIds.Contains(g.Id)));
+
+			var items = await query
+				.Skip(page * pageSize)
+				.Take(pageSize)
+				.AsNoTracking()
+				.ToListAsync();
+
+			return new PagedResult<Movie>
+			{
+				Items = items,
+				PageNumber = page,
+				PageSize = pageSize,
+				TotalCount = await query.CountAsync()
+			};
+		}
+
+		public async Task<Movie?> GetMovieByIdAsync ( int id )
+		{
+			return await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+		}
+
+		public async Task<Movie?> GetMovieByIdWithAllAsync ( int id )
+		{
+			return await _context.Movies
+				.Include(r => r.Actors)
+				.Include(r => r.Genres)
+				.AsNoTracking()
+				.FirstOrDefaultAsync(r => r.Id == id);
+		}
+
+		public async Task AddMovieAsync ( Movie movie )
+		{
+			await _context.Movies.AddAsync(movie);
+		}
+
+		public void UpdateMovie ( Movie movie )
+		{
+			_context.Movies.Update(movie);
+		}
+
+		public void DeleteMovie ( Movie movie )
+		{
+			_context.Movies.Remove(movie);
+		}
+	}
+}
