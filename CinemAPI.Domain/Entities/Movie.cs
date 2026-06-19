7bbcc12299a24a0bbe069c488b963a1f191322b5
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-19 15:22:15 +0200

    fix: add list of Actors in Movies

--- CinemAPI.Domain/Entities/Movie.cs
+++ CinemAPI.Domain/Entities/Movie.cs
@@ -6,6 +6,7 @@ namespace CinemAPI.Domain.Entities
 		public int Id { get; set; }
 		public string Title { get; set; } = string.Empty;
 		public string Description { get; set; } = string.Empty;
+		public IEnumerable<Actor> Actors { get; set; } = [];
 		public IEnumerable<Genre> Genres { get; set; } = [];
 		public DateOnly ReleaseYear { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 	}
