a9e633a526ee1c141fc2c6ad4e683cc530a62a4f
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-19 04:43:20 +0200

    Fix: change parameters of GetAllAsync method in IGenericRepository

--- CinemAPI.Infrastructure/Interfaces/IGenericRepository.cs
+++ CinemAPI.Infrastructure/Interfaces/IGenericRepository.cs
@@ -4,7 +4,7 @@ namespace CinemAPI.Infrastructure.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<PagedResult<T>> GetAllAsync();
+        Task<PagedResult<T>> GetAllAsync( int page, int pageSize = 2 );
         Task<T?> GetByIdAsync(int id);
         Task AddAsync(T obj);
         void Update(T obj);
