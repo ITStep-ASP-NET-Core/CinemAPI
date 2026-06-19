dcc1484b229299fa16b94156c8e53139e76793a6
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-19 16:05:50 +0200

    feat: add Movie filter DTO model

---
+++ CinemAPI.Application/DTO/Movie/MovieFilterDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CinemAPI.Application.DTO.Movie
+{
+	public class MovieFilterDto
+	{
+		public string? SearchQuery { get; set; }
+		public IEnumerable<int>? ActorIds { get; set; }
+		public IEnumerable<int>? GenreIds { get; set; }
+	}
+}
