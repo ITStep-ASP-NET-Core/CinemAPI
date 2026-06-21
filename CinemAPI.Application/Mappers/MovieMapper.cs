5a4d4d36613270fccc52ae966ef6a803f4ff4806
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-21 18:59:34 +0200

    feat: add mappers for application DTOs models

---
+++ CinemAPI.Application/Mappers/MovieMapper.cs
@@ -0,0 +1,34 @@
+﻿using CinemAPI.Application.DTO.Movie;
+using CinemAPI.Domain.Entities;
+
+namespace CinemAPI.Application.Mappers
+{
+	public class MovieMapper
+	{
+		public static MovieDto ToDto ( Movie movie ) => new()
+		{
+			Id = movie.Id,
+			Title = movie.Title,
+			ReleaseYear = movie.ReleaseYear,
+			ActorIds = movie.Actors.Select( a => a.Id ).ToList(),
+			GenreIds = movie.Genres.Select( a => a.Id ).ToList(),
+		};
+
+		public static MovieDetailDto ToDetailDto ( Movie movie ) => new()
+		{
+			Id = movie.Id,
+			Title = movie.Title,
+			Description = movie.Description,
+			ReleaseYear = movie.ReleaseYear,
+			Actors = movie.Actors.Select(ActorMapper.ToDto).ToList(),
+			Genres = movie.Genres.Select(GenreMapper.ToDto).ToList(),
+		};
+
+		public static Movie ToEntity ( MovieCreateDto movieDto ) => new()
+		{
+			Title = movieDto.Title,
+			Description = movieDto.Description,
+			ReleaseYear = movieDto.ReleaseYear,
+		};
+	}
+}
