52ed95405de7b1cdbcd5928d767d59233912fa5d
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-21 18:58:28 +0200

    feat: add initial application interfaces

---
+++ CinemAPI.Application/Interfaces/IMovieService.cs
@@ -0,0 +1,17 @@
+﻿
+using CinemAPI.Application.Common;
+using CinemAPI.Application.DTO.Movie;
+using CinemAPI.Domain.Common;
+
+namespace CinemAPI.Application.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<PagedResult<MovieDto>> GetMoviesAsync(int page);
+        Task<PagedResult<MovieDto>> GetMoviesByFiltersAsync(MovieFilterDto filter, int page);
+        Task<MovieDetailDto?> GetMovieByIdAsync(int id);
+        Task<Result> CreateMovieAsync(MovieCreateDto movieDto);
+        Task<Result> EditMovieAsync(MovieEditDto movieDto);
+        Task<Result> DeleteMovieAsync(int id);
+    }
+}
