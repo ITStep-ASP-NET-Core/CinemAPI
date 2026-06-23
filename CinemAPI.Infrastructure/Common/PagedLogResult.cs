90928a9b5254adf03448f4cfb5bac6b00002ad2c
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-24 01:59:11 +0200

    feat: add logging models

---
+++ CinemAPI.Infrastructure/Common/PagedLogResult.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemAPI.Infrastructure.Common
+{
+	public class PagedLogResult<T> where T : class
+	{
+		public ICollection<T> Items { get; set; } = [];
+		public string? ContinuationToken { get; set; }
+	}
+}
