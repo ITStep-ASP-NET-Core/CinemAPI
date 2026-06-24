6c5ba9a7308714ddb97bababb0e0b487bd225a59
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-24 02:02:12 +0200

    feat: add log service

---
+++ CinemAPI.Application/Implementations/LogService.cs
@@ -0,0 +1,40 @@
+﻿using CinemAPI.Application.DTO.Logger;
+using CinemAPI.Application.Interfaces;
+using CinemAPI.Application.Mappers;
+using CinemAPI.Infrastructure.Common;
+using CinemAPI.Infrastructure.Entities;
+using CinemAPI.Infrastructure.Interfaces;
+
+namespace CinemAPI.Application.Implementations
+{
+    public class LogService : ILogService
+    {
+		private readonly ILogRepository _logs;
+
+		public LogService ( ILogRepository logs )
+		{
+			_logs = logs;
+		}
+
+		public async Task<PagedLogResult<LogEntityDto>> GetAllAsync ( LogFilterDto? filter, string? continuationToken = null, CancellationToken ct = default )
+		{
+			var source = await _logs.GetAsync(filter?.Entity, filter?.Method, continuationToken, ct);
+			var logs = source.Items.Select(LogMapper.ToDto).ToList();
+
+			_ = _logs.LogAsync(new LogEntity
+			{
+				PartitionKey = "log",
+				Method = "view",
+				Action = "GetAllAsync",
+				Entity = "log",
+				Message = $"Logs viewed by {(filter?.Entity != null ? filter?.Entity : "_")}{(filter?.Method != null ? $", {filter?.Method}" : ", _")}"
+			});
+
+			return new PagedLogResult<LogEntityDto>
+			{
+				Items = logs,
+				ContinuationToken = source.ContinuationToken,
+			};
+		}
+	}
+}
---
+++ CinemAPI.Application/Interfaces/ILogService.cs
@@ -0,0 +1,11 @@
+﻿using CinemAPI.Application.Common;
+using CinemAPI.Application.DTO.Logger;
+using CinemAPI.Infrastructure.Common;
+
+namespace CinemAPI.Application.Interfaces
+{
+    public interface ILogService
+	{
+		Task<PagedLogResult<LogEntityDto>> GetAllAsync ( LogFilterDto? filter, string? continuationToken = null, CancellationToken ct = default );
+	}
+}
