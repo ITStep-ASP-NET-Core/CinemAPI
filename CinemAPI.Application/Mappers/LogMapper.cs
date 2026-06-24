43868b1f3acd7bc257ae9842d099e9409a3bfb90
CutoffSquire917 <pushkin080109@gmail.com>
2026-06-24 02:01:51 +0200

    feat: add log mapper

---
+++ CinemAPI.Application/Mappers/LogMapper.cs
@@ -0,0 +1,17 @@
+﻿using CinemAPI.Application.DTO.Logger;
+using CinemAPI.Infrastructure.Entities;
+
+namespace CinemAPI.Application.Mappers
+{
+	public class LogMapper
+	{
+		public static LogEntityDto ToDto ( LogEntity log ) => new()
+		{
+			Method = log.Method,
+			Action = log.Action,
+			Entity = log.Entity,
+			Message = log.Message,
+			OccurredAt = log.OccurredAt
+		};
+	}
+}
