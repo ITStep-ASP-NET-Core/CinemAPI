43398d6ae9710af69d8dcc4e4ef6e970a2fd47b8
CutoffSquire917 <pushkin080109@gmail.com>
2026-07-01 01:15:35 +0200

    feat: add Chat repository

---
+++ CinemAPI.Infrastructure/Interfaces/IChatRepository.cs
@@ -0,0 +1,9 @@
+﻿using OpenAI.Chat;
+
+namespace CinemAPI.Infrastructure.Interfaces
+{
+	public interface IChatRepository
+	{
+		Task<ChatCompletion> AnalyzeAsync ( string prompt, string imageUrl );
+	}
+}
